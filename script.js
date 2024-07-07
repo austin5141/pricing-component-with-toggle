916554582e9c2db523ca8249e3e60abb845a4d84
BuildTools <unconfigured@null.spigotmc.org>
2024-07-07 17:13:49 -0400

    Third Commit

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ rightPrice = document.querySelector(".rightPrice")
 btn = document.querySelector(".priceChanger")
 circle = document.querySelector(".circle")
 
-monthlyBasic = "$19.99"
-yearlyBasic = "$199.99"
+monthlyBasic = "19.99"
+yearlyBasic = "199.99"  
 
-monthlyPro = "$24.99"
-yearlyPro = "$249.99"
+monthlyPro = "24.99"
+yearlyPro = "249.99"
 
-monthlyMaster = "$39.99"
-yearlyMaster = "$399.99"
+monthlyMaster = "39.99"
+yearlyMaster = "399.99"
 
 let monthly = true
 
 btn.addEventListener("click", (e) => {
-    circle.classList.toggle("checked")
+
     if (monthly == true) {
-        leftPrice.innerText = yearlyBasic
-        midPrice.innerText = yearlyPro
-        rightPrice.innerText = yearlyMaster
+        leftPrice.textContent = yearlyBasic
+        midPrice.textContent = yearlyPro
+        rightPrice.textContent = yearlyMaster
         monthly = false
     } else {
-        leftPrice.innerText = monthlyBasic
-        midPrice.innerText = monthlyPro
-        rightPrice.innerText = monthlyMaster
+        leftPrice.textContent = monthlyBasic
+        midPrice.textContent = monthlyPro
+        rightPrice.textContent = monthlyMaster
         monthly = true
     }
 })
